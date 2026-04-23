d44c4dc9977520433489269e105b093a705ec0f1
Keriyu0614 <laithithaoly1315@gmai.com>
2026-04-23 13:12:09 +0700

    lab6

---
+++ ASC.Web/Areas/Identity/Pages/Account/InitiateResetPassword.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ASC.Utilities; // Chứa extension GetCurrentUserDetails()
+using Microsoft.AspNetCore.Http;
+
+namespace ASC.Web.Areas.Identity.Pages.Account
+{
+    public class InitiateResetPasswordModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IEmailSender _emailSender;
+
+        public InitiateResetPasswordModel(UserManager<IdentityUser> userManager, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _emailSender = emailSender;
+        }
+
+        public void OnGet()
+        {
+            // Để trống theo thiết kế
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            // 1. Tìm User dựa trên Email của người dùng hiện tại
+            var userEmail = HttpContext.User.GetCurrentUserDetails().Email;
+            var user = await _userManager.FindByEmailAsync(userEmail);
+
+            if (user == null)
+            {
+                return RedirectToPage("/Account/Login");
+            }
+
+            // 2. Tạo mã Token để Reset mật khẩu
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+            // 3. Tạo link callback dẫn tới trang ResetPassword thực sự
+            var callbackUrl = Url.Page(
+                "/Account/ResetPassword",
+                pageHandler: null,
+                values: new { userId = user.Id, code = code },
+                protocol: Request.Scheme);
+
+            // 4. Gửi Email thông báo
+            await _emailSender.SendEmailAsync(userEmail, "Reset Password",
+                $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
+
+            // 5. Chuyển hướng sang trang xác nhận đã gửi email
+            return RedirectToPage("./ResetPasswordEmailConfirmation");
+        }
+    }
+}
---
+++ ASC.Web/Program.cs
@@ -0,0 +1,114 @@
+﻿using ASC.Solution.Services;
+using ASC.Web.Configuration;
+using ASC.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ASC.Web.Services;
+using ASC.DataAccess.Interfaces;
+using ASC.DataAccess;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// 2. Identity + Google Authentication
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+// Chỉ đăng ký Google nếu có ClientId
+var googleClientId = builder.Configuration["Authentication:Google:ClientId"];
+var googleClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+
+if (!string.IsNullOrEmpty(googleClientId) && !string.IsNullOrEmpty(googleClientSecret))
+{
+    //builder.Services.AddAuthentication()
+    //    .AddGoogle(options =>
+    //    {
+    //        options.ClientId = googleClientId;
+    //        options.ClientSecret = googleClientSecret;
+    //    });
+    builder.Services.AddAuthentication() 
+        .AddGoogle(options =>
+        {
+            options.ClientId = googleClientId;
+            options.ClientSecret = googleClientSecret;
+        });
+}
+
+// 3. Custom DI + Config
+builder.Services.AddCongfig(builder.Configuration)
+                .AddMyDependencyGroup();
+
+
+// 4. MVC + Razor
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+// 5. Session
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// 6. Exception filter
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areaRoute",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+// 7. Seed data
+using (var scope = app.Services.CreateScope())
+{
+    var storageSeed = scope.ServiceProvider.GetRequiredService<IIdentitySeed>();
+    await storageSeed.Seed(
+        scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>(),
+        scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>(),
+        scope.ServiceProvider.GetRequiredService<IOptions<ApplicationSettings>>()
+    );
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var navigationCacheOperations = scope.ServiceProvider.GetRequiredService<INavigationCacheOperations>();
+    await navigationCacheOperations.CreateNavigationCacheAsync();
+}
+
+app.Run();
